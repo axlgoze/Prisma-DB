694d13497eb6a31c5204c488e6392a5f64484aec
Axel Reyes <rbaxel.f@gmail.com>
2022-06-22 21:59:56 -0500

    solucionando cors

--- server.js
+++ server.js
@@ -13,12 +13,14 @@ app.get('/', (req, res) => {
 
 app.get('/explorers', async (req, res) => {
   const allExplorers =  await prisma.explorer.findMany({});
+  res.set("Access-Control-Allow-Origin", "*");
   res.json(allExplorers);
 });
 
 app.get('/explorers/:id', async (req, res) => {
   const id = req.params.id;
   const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
+  res.set("Access-Control-Allow-Origin", "*");
   res.json(explorer);
 });
 
@@ -30,6 +32,7 @@ app.post('/explorers', async (req, res) => {
    };
   const message = 'Explorer creado.';
   await prisma.explorer.create({data: explorer});
+  res.set("Access-Control-Allow-Origin", "*");
   return res.json({message});
 });
 
@@ -44,16 +47,26 @@ app.put('/explorers/:id', async (req, res) => {
 			mission: req.body.mission
 		}
 	})
-
+  res.set("Access-Control-Allow-Origin", "*");
 	return res.json({message: "Actualizado correctamente"});
 });
 
 app.delete('/explorers/:id', async (req, res) => {
 	const id = parseInt(req.params.id);
 	await prisma.explorer.delete({where: {id: id}});
+  res.set("Access-Control-Allow-Origin", "*");
 	return res.json({message: "Eliminado correctamente"});
 });
 
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
+
+//cors
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:8081",
+  optionsSuccessStatus: 200,
+};
+app.use(cors());
+app.options("*", cors());
