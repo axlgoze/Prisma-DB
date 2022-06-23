e3444d71deeaa3cd8b22e4fabfcea6be2752fffa
Axel Reyes <rbaxel.f@gmail.com>
2022-06-22 19:26:45 -0500

    agregando endpoint para regresar registro por id

--- server2.js
+++ server2.js
@@ -15,6 +15,18 @@ app.get('/exploradores', async (req,res)=>{
   res.json(allExploradores);
 });
 
+//GET para regresar el registro dado el ID
+app.get('/exploradores/:id', async (req,res)=>{
+  const id=req.params.id;
+  const explorador = await prisma.explorador.findUnique({where:{id: parseInt(id)}});
+  res.json(explorador);
+});
+
+//POST para crear nuevo registro
+app.get('',(req,res)=>{
+
+});
+
 
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
